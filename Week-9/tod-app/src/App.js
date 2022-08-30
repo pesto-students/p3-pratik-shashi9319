b5c3f578393bc6f7869daae2f1d4c7d5f1ce46dc
shashi9319 <107118244+shashi9319@users.noreply.github.com>
2022-08-30 17:13:00 +0530

    Todo app created
    completed todo app, need review on some of the concepts like useEffect

---
+++ Week-9/tod-app/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+    }
+  };
+  return (
+    <div className="App">
+      <header>My Todo</header>
+      <Form
+        inputText={inputText}
+        setTodos={setTodos}
+        todos={todos}
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+      <TodoList
+        setTodos={setTodos}
+        todos={todos}
+        filteredTodos={filteredTodos}
+      />
+    </div>
+  );
+}
+
+export default App;
